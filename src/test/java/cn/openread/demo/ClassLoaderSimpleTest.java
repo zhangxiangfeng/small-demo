976da48a1f81259113c5fbcb8cd0cfefea08fc8f
速道科技 <zhangxiangfeng@kuaicto.com>
2018-11-05 10:03:17 +0800

    dev:
    1.什么是类加载器
    2.有什么类加载器
    3.什么双亲委派？
    
    Signed-off-by: 速道科技 <zhangxiangfeng@kuaicto.com>

---
+++ src/test/java/cn/openread/demo/ClassLoaderSimpleTest.java
@@ -0,0 +1,64 @@
+package cn.openread.demo;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @see https://blog.csdn.net/u011080472/article/details/51332866
+ * @see http://www.cnblogs.com/lanxuezaipiao/p/4138511.html
+ * <p>
+ * 加载类的开放性
+ * <p>
+ * 类加载是获取一个类的第一步，Java并没有规定应该怎么去获取？去哪里获取
+ * 所以有很多方式
+ * 1.压缩包
+ * 2.网络
+ * 3.动态代理
+ * 4.其它文件生成例如JSP
+ * <p>
+ * <p>
+ * 类加载器与类的唯一性
+ * <p>
+ * 不同类加载器加载出来同一个class文件那么他们也是不等的
+ * <p>
+ * 这里的“相等”，包括代表类的Class对象的equals()方法、isAssignableFrom()方法、isInstance()方法的返回结果，也包括使用instanceof关键字做对象所属关系判定等情况。
+ */
+public class ClassLoaderSimpleTest {
+
+
+    @Test
+    public void test() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        // 自定义类加载器
+        ClassLoader myLoader = new ClassLoader() {
+            @Override
+            public Class<?> loadClass(String name) throws ClassNotFoundException {
+                try {
+                    String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+                    InputStream is = getClass().getResourceAsStream(fileName);
+                    if (is == null) {
+                        return super.loadClass(name);
+                    }
+                    byte[] b = new byte[is.available()];
+                    is.read(b);
+                    return defineClass(name, b, 0, b.length);
+                } catch (IOException e) {
+                    throw new ClassNotFoundException();
+                }
+            }
+        };
+
+
+        // 使用ClassLoaderTest的类加载器加载本类
+        Object obj1 = ClassLoaderSimpleTest.class.getClassLoader().loadClass("cn.openread.demo.ClassLoaderSimpleTest").newInstance();
+        System.out.println(obj1.getClass());
+        System.out.println(obj1 instanceof cn.openread.demo.ClassLoaderSimpleTest);
+
+
+        // 使用自定义类加载器加载本类
+        Object obj2 = myLoader.loadClass("cn.openread.demo.ClassLoaderSimpleTest").newInstance();
+        System.out.println(obj2.getClass());
+        System.out.println(obj2 instanceof cn.openread.demo.ClassLoaderSimpleTest);
+    }
+}
