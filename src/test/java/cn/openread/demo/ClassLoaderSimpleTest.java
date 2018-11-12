d48b4734a1e1aacec1c13c1f6054e5d43da9c62f
Simon <812135023@qq.com>
2018-11-12 23:40:26 +0800

    dev: 1.再次理解双亲委派机制

--- src/test/java/cn/openread/demo/ClassLoaderSimpleTest.java
+++ src/test/java/cn/openread/demo/ClassLoaderSimpleTest.java
@@ -24,6 +24,10 @@ import java.io.InputStream;
  * 不同类加载器加载出来同一个class文件那么他们也是不等的
  * <p>
  * 这里的“相等”，包括代表类的Class对象的equals()方法、isAssignableFrom()方法、isInstance()方法的返回结果，也包括使用instanceof关键字做对象所属关系判定等情况。
+ * <p>
+ * 那么什么是双亲委派？
+ * 某个特定的类加载器在接到加载类的请求时，首先将加载任务委托给父类加载器，依次递归，如果父类加载器可以完成类加载任务，就成功返回；
+ * 只有父类加载器无法完成此加载任务时，才自己去加载。
  */
 public class ClassLoaderSimpleTest {
 
