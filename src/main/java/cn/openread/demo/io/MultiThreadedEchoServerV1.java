a6cf1e40d66bf70f5116f1e12440dba61393c6c7
速道科技 <zhangxiangfeng@kuaicto.com>
2018-11-22 18:20:38 +0800

    dev: 1.Java IO和NIO加深理解
    Signed-off-by: 速道科技 <zhangxiangfeng@kuaicto.com>

---
+++ src/main/java/cn/openread/demo/io/MultiThreadedEchoServerV1.java
@@ -0,0 +1,34 @@
+package cn.openread.demo.io;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class MultiThreadedEchoServerV1 {
+    private int port;
+
+    public MultiThreadedEchoServerV1(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws IOException {
+        new MultiThreadedEchoServerV1(8080).startServer();
+    }
+
+    public void startServer() {
+        ServerSocket echoServer = null;
+        int i = 0;
+        System.out.println("服务器在端口[" + this.port + "]等待客户请求......");
+        try {
+            echoServer = new ServerSocket(8080);
+            while (true) {
+                Socket clientRequest = echoServer.accept();
+                new Thread(new ThreadedServerHandler(clientRequest, i++)).start();
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+}
